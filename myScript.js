5db02169eddf71402f4ce871265aca12bd6f860a
edodima13 <edodima13@libero.it>
2022-02-03 16:17:39 +0100

    avanzamento

---
+++ myScript.js
@@ -0,0 +1,6 @@
+const userSecondName = prompt("Inserisci il tuo cognome, Di Martino")
+console.log(userSecondName);
+
+//let userSecondName = prompt("Inserisci il tuo nome, Edoardo")
+
+//content.innerHTML = userSecondName
