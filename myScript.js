1f985445834a1e2d321bd44281f4442cee1423d8
edodima13 <edodima13@libero.it>
2022-02-03 16:45:54 +0100

    avanzamneto

--- myScript.js
+++ myScript.js
@@ -4,7 +4,10 @@ console.log(userSecondName);
 const userFirstName = prompt("Inserisci il tuo cognome")
 console.log(userFirstName);
 
+const userFavoriteColor = prompt("Dimmi il tuo colore preferito")
+console.log(userFirstName);
+
 const salutoHTMLElement = document.getElementById("saluto")
 
-salutoHTMLElement.innerHTML += userSecondName + ' ' + userFirstName
+salutoHTMLElement.innerHTML += userSecondName + ' ' + userFirstName + ' ' + userFavoriteColor 
 
