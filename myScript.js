ab79abe2fb92752fe18e54d1faaa6d5275ac3c1c
edodima13 <edodima13@libero.it>
2022-02-03 17:08:27 +0100

    inserimento password

--- myScript.js
+++ myScript.js
@@ -1,3 +1,5 @@
+//domande-utente
+
 const userSecondName = prompt("Inserisci il tuo nome")
 console.log(userSecondName);
 
@@ -5,9 +7,20 @@ const userFirstName = prompt("Inserisci il tuo cognome")
 console.log(userFirstName);
 
 const userFavoriteColor = prompt("Dimmi il tuo colore preferito")
-console.log(userFirstName);
+console.log(userFavoriteColor);
 
 const salutoHTMLElement = document.getElementById("saluto")
 
-salutoHTMLElement.innerHTML += userSecondName + ' ' + userFirstName + ' ' + userFavoriteColor 
+salutoHTMLElement.innerHTML += userSecondName + userFirstName + userFavoriteColor
 
+//password-utente
+
+const SecondName = Nome
+console.log(passwordNumber);
+
+const passwordNumber = 21
+console.log(passwordNumber);
+
+const passwordHTMLElement = document.getElementById("password")
+
+passwordHTMLElement.innerHTML += SecondName + FirstName + FavoriteColor + passwordNumber
